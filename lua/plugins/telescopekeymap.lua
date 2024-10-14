78e86d4e924c4d422dde5247c1e4126a4a20524b
nzdev <nz@desk.com>
2024-10-14 22:02:51 +0700

    fix telescope

--- lua/plugins/telescopekeymap.lua
+++ lua/plugins/telescopekeymap.lua
@@ -2,7 +2,25 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      { "<leader><space>", LazyVim.pick("files"), desc = "Find Files (cwd)" },
+      { "<leader><leader>", LazyVim.pick("files"), desc = "Find Files (cwd)" },
     },
+    opts = function(_, opts)
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          -- Set ignore patterns for file search
+          file_ignore_patterns = { "node_modules" },
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+              -- Additional mappings here
+            },
+          },
+        },
+      })
+    end,
   },
 }
