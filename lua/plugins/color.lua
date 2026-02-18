f9e8148e1257bdda201045a3a8b7b67a30ab6dd5
nzlap <nz@gmail.com>
2026-02-19 00:12:56 +0700

    add last update

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -60,11 +60,11 @@ return {
     "craftzdog/solarized-osaka.nvim",
     lazy = true,
     priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
+    -- opts = function()
+    --   return {
+    --     transparent = true,
+    --   }
+    -- end,
   },
   {
     "catppuccin/nvim",
