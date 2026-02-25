70cbf57730b795e67503eda77e3ae8827c562816
nzlap <nz@gmail.com>
2026-02-25 14:27:02 +0700

    config snacks

---
+++ lua/plugins/disable.enable.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = false,
+  },
+  -- {
+  --   "folke/snacks.nivm",
+  --   {
+  --     opts = {
+  --       dim = { enabled = true },
+  --     },
+  --   },
+  -- },
+}
