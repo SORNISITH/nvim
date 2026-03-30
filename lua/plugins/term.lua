0689899ecd26a75ecbc8ec51c6cf162f98f4366a
void <void@gmail.com>
2026-03-30 15:27:24 +0700

    fix yazi and toggle term ctr + / full screen

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -1,3 +1,43 @@
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      local toggleterm = require("toggleterm")
+      toggleterm.setup({
+        start_in_insert = true, -- only on first open
+        open_mapping = false, -- custom mapping
+        direction = "float",
+        float_opts = {
+          border = "none",
+          width = function()
+            return vim.o.columns
+          end,
+          height = function()
+            return vim.o.lines - 1
+          end,
+          winblend = 0,
+        },
+      })
+
+      local key = vim.keymap.set
+
+      -- Normal toggle function (does not force insert every time)
+      local function toggle_term()
+        vim.cmd("ToggleTerm")
+      end
+
+      -- Keymaps
+      key("n", "<C-_>", toggle_term)
+      key("i", "<C-_>", function()
+        vim.cmd("stopinsert")
+        toggle_term()
+      end)
+      key("t", "<C-_>", toggle_term)
+
+      -- Terminal mode: go to normal mode
+      key("t", "<Esc>", [[<C-\><C-n>]], { desc = "Terminal Normal Mode" })
+      key("t", "<C-n>", [[<C-\><C-n>]], { desc = "Terminal Normal Mode" })
+    end,
+  },
 }
--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,5 +1,6 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
   {
     "mikavilpas/yazi.nvim",
     keys = {
@@ -9,5 +10,19 @@ return {
         desc = "Open Yazi file manager",
       },
     },
+    config = function()
+      require("yazi").setup({
+        layout = "float",
+        window = {
+          border = "none", -- no border
+          width = vim.o.columns, -- full width
+          height = vim.o.lines, -- full height
+          row = 0, -- top-left corner
+          col = 0,
+          winblend = 0, -- optional: transparency
+        },
+        auto_open = false,
+      })
+    end,
   },
 }
