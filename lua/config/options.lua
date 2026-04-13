6dc99270ec33a79386bed0276aa2e564cf2d1b72
void <void@gmail.com>
2026-04-13 13:58:11 +0700

    add block

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,25 @@
 vim.o.encoding = "utf-8"
 vim.o.fileencoding = "utf-8"
 vim.o.termguicolors = true
+-- Cursor behavior
+vim.opt.guicursor = table.concat({
+  "n:block", -- normal
+  "v:block", -- visual
+  "i:hor20-blinkon500-blinkoff500", -- insert = underline
+}, ",")
+
+-- Colors
+vim.api.nvim_set_hl(0, "Cursor", {
+  fg = "black",
+  bg = "#ff0000", -- red (normal)
+})
+
+vim.api.nvim_set_hl(0, "CursorVisual", {
+  fg = "black",
+  bg = "#ff69b4", -- pink (visual)
+})
+
+vim.api.nvim_set_hl(0, "CursorInsert", {
+  fg = "black",
+  bg = "#ffffff", -- white (insert)
+})
